ac75cdf13eaccf10dc10eaaab5bd39ad910d7871
yashrajjadhav2010 <yashrajart1999@gmail.com>
2026-03-10 10:53:51 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('v1').then((cache) => cache.addAll(['index.html', 'manifest.json']))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
